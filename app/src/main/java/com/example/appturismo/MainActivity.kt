997db0b65f9ae1619a4763c80eb7c26f9c582a35
INES_ROCIO <quizhpiines@gmail.com>
2026-07-21 17:12:11 -0500

    Actualización de build.gradle y MainActivity + nuevas pantallas

--- app/src/main/java/com/example/appturismo/MainActivity.kt
+++ app/src/main/java/com/example/appturismo/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appturismo.ui.theme.AppTurismoTheme
-
+import com.example.appturismo.ui.theme.screens.HomeScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTurismoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTurismoTheme {
-        Greeting("Android")
-    }
-}
