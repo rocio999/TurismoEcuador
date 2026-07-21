61ad31e116b6439549bf640a6efe7c91cd9c2f85
INES_ROCIO <quizhpiines@gmail.com>
2026-07-21 18:30:22 -0500

    Cambios en destinp

--- app/src/main/java/com/example/appturismo/ui/theme/screens/DestinosScreen.kt
+++ app/src/main/java/com/example/appturismo/ui/theme/screens/DestinosScreen.kt
@@ -1,33 +1,76 @@
 package com.example.appturismo.ui.theme.screens
 
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+data class Destino(
+    val nombre: String,
+    val descripcion: String
+)
 
 @Composable
 fun DestinosScreen() {
 
+    val destinos = listOf(
+        Destino("🏔️ Quilotoa", "Laguna de origen volcánico"),
+        Destino("🌋 Cotopaxi", "Parque Nacional"),
+        Destino("🌊 Montañita", "Playa del Ecuador")
+    )
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize()
     ) {
 
         Text(
-            text = "🏔️ Destinos Turísticos",
-            style = MaterialTheme.typography.headlineMedium
+            text = "Destinos Turísticos",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(16.dp)
         )
 
-        Text(
-            text = "Quilotoa\nCotopaxi\nAtacames\nEsmeraldas\nChimborazo"
-        )
+        LazyColumn(
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+            items(destinos) { destino ->
+
+                Card(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+
+                        Text(
+                            text = destino.nombre,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+
+                        Text(
+                            text = destino.descripcion
+                        )
+
+                    }
+
+                }
+
+            }
+
+        }
 
     }
+
 }
